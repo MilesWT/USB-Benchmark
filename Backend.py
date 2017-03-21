8261d2bc07803dc510db094d3db0e4308669954f
bdavs <rdavis2@umassd.edu>
2017-03-20 21:32:50 -0400

    connected function to button as proof of concept

--- Backend.py
+++ Backend.py
@@ -4,4 +4,50 @@ import time
 import numpy
 import pandas
 from PyQt5 import QtGui, QtWidgets, uic
-import Benchmarking #import the GUI layout
+import Benchmarking_input #import the GUI layout
+import Benchmarking_output #import the GUI layout
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+class InputUi(QtWidgets.QMainWindow, Benchmarking_input.Ui_MainWindow):
+
+    def __init__(self):
+        #initialization functions go here (including globals for this file):
+        QtWidgets.QMainWindow.__init__(self)
+        self.ui = Benchmarking_input.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # initialize globals here
+        self.globalVariables = 0
+
+        # All event connections will go here:
+        self.ui.runButton.pressed.connect(self.updateText)
+
+    def updateText(self):
+        # helper function to display 'running' while running application
+
+        self.ui.runButton.setText("Running")
+
+
+class OutputUi(QtWidgets.QMainWindow, Benchmarking_output.Ui_MainWindow):
+
+    def __init__(self):
+        #initialization functions go here (including globals for this file):
+        QtWidgets.QMainWindow.__init__(self)
+        self.ui = Benchmarking_output.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+
+if __name__ == '__main__':
+    # This is the first operation  to be run on startup.
+    # AKA the 'Main' function
+    app = QtWidgets.QApplication(sys.argv)
+    GUI_in = InputUi()
+    GUI_out = OutputUi()
+    ui = Benchmarking_input.Ui_MainWindow()
+    GUI_in.show()
+    GUI_out.show()
+    sys.exit(app.exec_())
