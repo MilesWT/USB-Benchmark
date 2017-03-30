a973fd3664808a12e7035f62fee2b8ef560430a5
bdavs <rdavis2@umassd.edu>
2017-03-30 00:13:55 -0400

    added a graph demo to the output, among other fixes

--- Backend.py
+++ Backend.py
@@ -30,18 +30,24 @@ class InputUi(QtWidgets.QMainWindow, Benchmarking_input.Ui_MainWindow):
         self.ui.lineEdit_3.setText(name3)
         self.ui.lineEdit_4.setText(name4)
 
+
         # initialize globals here
         self.globalVariables = 0
         self.devices = wmicAPI.getDevices()
+        self.radio_index = 0
+        self.prev_radio_index = 0
         self.ButtonGroup = QtWidgets.QButtonGroup()
         self.ButtonGroup.addButton(self.ui.radioButton_1, 0)
         self.ButtonGroup.addButton(self.ui.radioButton_2, 1)
         self.ButtonGroup.addButton(self.ui.radioButton_3, 2)
         self.ButtonGroup.addButton(self.ui.radioButton_4, 3)
         self.initDevices()
+        self.loadTest()
+
 
         # All event connections will go here:
-        self.ui.runButton.pressed.connect(self.updateText)
+        self.ui.runButton.pressed.connect(self.runButtonPressed)
+        self.ui.runButton.released.connect(self.runButtonReleased)
         self.ui.refresh.clicked.connect(self.initDevices)
         self.ui.horizontalSlider.valueChanged.connect(lambda: self.sliderText(self.ui.horizontalSlider))
         self.ui.horizontalSlider_2.valueChanged.connect(lambda: self.sliderText(self.ui.horizontalSlider_2))
@@ -53,16 +59,15 @@ class InputUi(QtWidgets.QMainWindow, Benchmarking_input.Ui_MainWindow):
 
         self.devices = wmicAPI.getDevices() #get the updated list of devices
 
-        for i in range(self.ui.comboBox.count(),1,-1): #remove the current list
+        for i in range(self.ui.comboBox.count(), 1, -1): #remove the current list
             self.ui.comboBox.removeItem(i)
 
         if len(self.devices):
             for i in range(0, len(self.devices['DeviceID'])): #update with new list
                 self.ui.comboBox.addItem(str(self.devices['DeviceID'][i]) + "  " + str(self.devices['VolumeName'][i]))
 
-    def updateText(self):
-        # helper function to display 'running' while running application
-        self.ui.runButton.setText("Running")
+    def addText(self,string):
+        self.ui.log.appendPlainText(string)
 
     def sliderText(self, slider):
         #update the slider text when it moves
@@ -73,30 +78,30 @@ class InputUi(QtWidgets.QMainWindow, Benchmarking_input.Ui_MainWindow):
             if slider.value() > self.ui.horizontalSlider_2.value():
                 self.ui.horizontalSlider_2.setValue(slider.value())
             self.ui.sliderLabelMin.setText(txt)
-            self.ui.log.appendPlainText("Lowest Block Size: "+txt)
+            self.addText("Lowest Block Size: " + txt)
         elif slider == self.ui.horizontalSlider_2:
             if slider.value() < self.ui.horizontalSlider.value():
                 self.ui.horizontalSlider.setValue(slider.value())
             self.ui.sliderLabelMax.setText(txt)
-            self.ui.log.appendPlainText("Highest Block Size: "+txt)
+            self.addText("Highest Block Size: " + txt)
 
     def radioButtonClick(self):
-        self.saveTest()
+        self.radioIndex()
+        self.saveTest(self.prev_radio_index)
         self.loadTest()
+        self.enableTextbox()
 
+    def saveTest(self, index=None):
+        if index is None:
+            index = self.radio_index
 
-    def saveTest(self):
-        if self.ui.radioButton_1.isChecked():
-            radio_index = 0
+        if index == 0:
             test_name = self.ui.lineEdit_1.text()
-        elif self.ui.radioButton_2.isChecked():
-            radio_index = 1
+        elif index == 1:
             test_name = self.ui.lineEdit_2.text()
-        elif self.ui.radioButton_3.isChecked():
-            radio_index = 2
+        elif index == 2:
             test_name = self.ui.lineEdit_3.text()
-        elif self.ui.radioButton_4.isChecked():
-            radio_index = 3
+        elif index == 3:
             test_name = self.ui.lineEdit_4.text()
         else:
             pass
@@ -104,31 +109,63 @@ class InputUi(QtWidgets.QMainWindow, Benchmarking_input.Ui_MainWindow):
         slider_max = self.ui.horizontalSlider_2.value()
         read_checkbox = self.ui.readCheckBox.isChecked()
         write_checkbox = self.ui.writeCheckBox.isChecked()
-        save_state.save_state(radio_index, test_name, slider_min, slider_max, read_checkbox, write_checkbox)
-
+        save_state.save_state(index, test_name, slider_min, slider_max, read_checkbox, write_checkbox)
+        self.addText("\r\nSaved Test: " + test_name)
 
     def loadTest(self):
-
-        radio_index = [self.ButtonGroup.buttons()[x].isChecked() for x in
-                          range(len(self.ButtonGroup.buttons()))].index(True)
-
-        name, slider_min, slider_max, read_checkbox, write_checkbox = save_state.load_radio_button(radio_index)
+        name, slider_min, slider_max, read_checkbox, write_checkbox = save_state.load_radio_button(self.radio_index)
 
         self.ui.horizontalSlider.setValue(int(slider_min))
         self.ui.horizontalSlider_2.setValue(int(slider_max))
         self.ui.readCheckBox.setChecked(literal_eval(read_checkbox))
         self.ui.writeCheckBox.setChecked(literal_eval(write_checkbox))
 
+    def radioIndex(self):
+        self.prev_radio_index = self.radio_index
+        self.radio_index = [self.ButtonGroup.buttons()[x].isChecked() for x in
+                            range(len(self.ButtonGroup.buttons()))].index(True)
+
+    def enableTextbox(self):
+        self.ui.lineEdit_1.setEnabled(False)
+        self.ui.lineEdit_2.setEnabled(False)
+        self.ui.lineEdit_3.setEnabled(False)
+        self.ui.lineEdit_4.setEnabled(False)
+
+        if self.radio_index == 0:
+            self.ui.lineEdit_1.setEnabled(True)
+        elif self.radio_index == 1:
+            self.ui.lineEdit_2.setEnabled(True)
+        elif self.radio_index == 2:
+            self.ui.lineEdit_3.setEnabled(True)
+        elif self.radio_index == 3:
+            self.ui.lineEdit_4.setEnabled(True)
+        else:
+            pass
+
+    def runButtonPressed(self):
+        # helper function to display 'running' while running application
+        self.ui.runButton.setText("Running")
+
+    def runButtonReleased(self):
+        GUI_out.show()
 
 class OutputUi(QtWidgets.QMainWindow, Benchmarking_output.Ui_MainWindow):
 
     def __init__(self):
         #initialization functions go here (including globals for this file):
         QtWidgets.QMainWindow.__init__(self)
-        self.ui = Benchmarking_output.Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.ui_2 = Benchmarking_output.Ui_MainWindow()
+        self.ui_2.setupUi(self)
+        pixmap = QtGui.QPixmap('Resources/demo_graph.png')
+        self.ui_2.Graph.setPixmap(pixmap)#.scaled(550,225))
 
-
+    def setParameters(self):
+        self.ui_2.deviceName.setText("boooobs")
+        self.ui_2.size.setText("boooobs")
+        self.ui_2.serialNumber.setText("boooobs")
+        self.ui_2.mountPoint.setText("boooobs")
+        self.ui_2.format.setText("boooobs")
+        self.ui_2.freeSpace.setText("boooobs")
 
 if __name__ == '__main__':
     # This is the first operation  to be run on startup.
